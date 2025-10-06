6ed8b4d505138efa3b578a2ade2deb2d5a59c238
RomanPrikazstikov <567prik@gmail.com>
2025-10-06 10:35:21 +0300

    execute ja drop index

--- IndeksidServeris.sql
+++ IndeksidServeris.sql
@@ -3,3 +3,7 @@ Select * from DimEmployee where BaseRate > 10 and BaseRate < 15
 
 Create Index IX_DimEmployee_BaseRate
 ON DimEmployee (BaseRate ASC)
+
+Execute sp_helptext DimEmployee
+
+Drop Index DimEmployee.IX_DimEmployee_BaseRate
