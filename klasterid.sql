e84b151411bdcbea96a34d9510af5586d93407b7
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-10-06 10:50:08 +0300

    indeksid kood transferred

--- klasterid.sql
+++ klasterid.sql
@@ -1,3 +1,13 @@
+--Indeksid Serveris ulesanned
+Select * from DimEmployee where BaseRate > 10 and BaseRate < 15
+
+Create Index IX_DimEmployee_BaseRate
+ON DimEmployee (BaseRate ASC)
+
+Execute sp_helptext DimEmployee
+
+Drop Index DimEmployee.IX_DimEmployee_BaseRate
+
 --Klastreeritud ja mitte-klastreeritud indeksid
 Create Clustered Index IX_Employee_FirstName
 ON DimEmployee(FirstName)
