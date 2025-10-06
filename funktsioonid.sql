f5e08baffa2dac9240a17d686315b9aa0905ddb3
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-10-06 09:09:17 +0300

    funktsioonid tehtud.sql

--- funktsioonid.sql
+++ funktsioonid.sql
@@ -9,3 +9,21 @@ Return (Select EmployeeKey,FirstName, Cast(BirthDate as Date) as DOB
 from dbo.DimEmployee);
 
 select * from fn_ILTVF_GetEmployees();
+
+--Õpilane teeb päringu ja näitab koodi kirja ning tulemust pildi kujul.
+Create Function fn_MSTVF_GetEmployees()
+
+Returns @Table Table (EmployeeKey int,FirstName varchar(20), DOB Date)
+as
+begin
+Insert into @Table
+Select EmployeeKey,FirstName, Cast(BirthDate as Date)
+from dbo.DimEmployee
+
+Return
+End
+
+select * from fn_ILTVF_GetEmployees()
+select * from fn_MSTVF_GetEmployees()
+
+Update fn_ILTVF_GetEmployees()set FirstName="Sam1" Where id=1
