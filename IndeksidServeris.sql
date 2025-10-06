cb802dfc2cb017b4aefb4aeedaed540da622719d
RomanPrikazstikov <567prik@gmail.com>
2025-10-06 10:52:04 +0300

    kustutatud fail ja renamed

--- IndeksidServeris.sql
+++
@@ -1,9 +0,0 @@
---Indeksid Serveris ulesanned
-Select * from DimEmployee where BaseRate > 10 and BaseRate < 15
-
-Create Index IX_DimEmployee_BaseRate
-ON DimEmployee (BaseRate ASC)
-
-Execute sp_helptext DimEmployee
-
-Drop Index DimEmployee.IX_DimEmployee_BaseRate
