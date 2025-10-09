d654923edf534723a567de207d84005343fcf38b
Opilane TTHK <opilane@tthk.ee>
2025-10-09 14:33:00 +0300

    teine view lisatud

--- view.sql
+++ view.sql
@@ -1,5 +1,16 @@
---View SQL serveris
+--VIEWS
 SELECT EmployeeKey, FirstName, BaseRate, Gender, DepartmentName
 FROM DimEmployee
 JOIN DimDepartmentGroup
 ON DimEmployee.DepartmentName = DimDepartmentGroup.DepartmentGroupKey
+
+create view vwemployeesbydepartment
+as
+select employeekey, firstname, baserate, gender, departmentname
+from dimemployee
+join dimdepartmentgroup
+on dimemployee.departmentname = dimdepartmentgroup.departmentgroupname;
+
+select * from vwemployeesbydepartment;
+
+select * from DimEmployee
