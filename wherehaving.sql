c1f4ef0c878b85ad1d4536fbcdcb9afbe54b57fc
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-10-09 15:49:06 +0300

    unitprice.sql

--- wherehaving.sql
+++ wherehaving.sql
@@ -3,3 +3,9 @@ select * from factinternetsales
 select productkey, sum(unitprice) as totalsales
 from factinternetsales
 group by productkey
+
+
+select productkey, sum(unitprice) as totalsales
+from factinternetsales
+group by productkey
+having sum (unitprice) > 1000
